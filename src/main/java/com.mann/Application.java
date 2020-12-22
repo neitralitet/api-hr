676869970c2f6a0162c1f0b6d2c3fa9085716662
neitralitet <vladimirmanna@gmail.com>
2020-12-23 02:52:47 +0500

    для томката

--- src/main/java/com.mann/Application.java
+++ src/main/java/com.mann/Application.java
@@ -2,6 +2,7 @@ package com.mann;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application {
+public class Application  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
