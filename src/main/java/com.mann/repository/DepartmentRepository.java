96f8655d17a3dcb0c5bcda816b984cb152f5cc33
neitralitet <vladimirmanna@gmail.com>
2020-12-23 16:33:26 +0500

    Repository

--- src/main/java/com.mann/repository/DepartmentRepository.java
+++ src/main/java/com.mann/repository/DepartmentRepository.java
@@ -2,9 +2,11 @@ package com.mann.repository;
 
 import com.mann.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Vladimir Mann
  */
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 }
